178f7e72eb9e0866e266a7882e97e8c70f521597
rbentivenha <bentivenha@gmail.com>
2020-01-23 11:43:47 -0300

    Did a fib sequence

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,26 @@
 use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
 
 fn main () {
-    println! ("Guess the number!");
-    let _secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("Please the nth element of fibonnaci");
+
+    let mut guess = String::new();
+    let mut n1 = 0;
+    let mut n2 = 1;
+    let mut count = 1;
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    let guess : u32 = guess.trim().parse()
+        .expect("Type a number, please");
 
     loop {
-
-        println!("Please input a number");
-
-        let mut guess = String::new();
-
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
-        let guess : u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&_secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
+        if count == guess {
+            break;
         }
+        let actual = n1 + n2;
+        n1 = n2;
+        n2 = actual;
+        count = count + 1;
     }
 
-    println!("The secret number was: {}", _secret_number);
+    println!("Nth: {}", n2);
 }
